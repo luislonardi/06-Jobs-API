a1a9bb9d57e58282fb3c6b3a7c0a4b6c71b1f899
Luis Lonardi <luis.lonardi@licitacon.com.ar>
2022-05-16 18:34:59 -0300

    16 de mayo

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,3 +1,7 @@
+const Job=require('../models/Job')
+const {StatusCodes}= require('http-status-codes')
+const {BadRequestError,NotFoundError}= require('../errors/index')
+
 const getAllJobs= async (req,res)=>{
     res.send('get all jobs')
 }
@@ -7,7 +11,10 @@ const getJob= async (req,res)=>{
 }
 
 const createJob= async (req,res)=>{
-    res.json(req.user)
+    req.body.createdBy=req.user.userId
+    console.log(req.body)
+    console.log(req.user)
+    // const job=await job.create(req.body)
     
 }
 
