55eb3d39085bfc9568ecd6efef725c8ea91af56a
Luis Lonardi <luis.lonardi@licitacon.com.ar>
2022-05-17 21:07:39 -0300

    17 mayo noche

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,20 +1,25 @@
 const Job=require('../models/Job')
 const {StatusCodes}= require('http-status-codes')
 const {BadRequestError,NotFoundError}= require('../errors/index')
+const { json } = require('express/lib/response')
 
 const getAllJobs= async (req,res)=>{
-    res.send('get all jobs')
+    const jobs=await Job.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs, count: jobs.length})
+    
 }
 
 const getJob= async (req,res)=>{
-    res.send('get job')
+    const job=Job.findByOne({_id:req.params})
+    console.log(job)
+    res.status(StatusCodes.OK).json({job})
 }
 
 const createJob= async (req,res)=>{
     req.body.createdBy=req.user.userId
-    console.log(req.body)
-    console.log(req.user)
-    // const job=await job.create(req.body)
+    const job= await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
+    
     
 }
 
